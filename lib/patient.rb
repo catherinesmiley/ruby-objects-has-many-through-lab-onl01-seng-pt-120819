d679eabef1f8bb00d575c654544f44d7b3a138ae
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-25 03:39:41 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,7 +14,17 @@ class Patient
   end
   
   def new_appointment(doctor, date)
-    Appointment.new()
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments 
+    Appointment.all.select do |appointment|
+      appointment.patient == self 
+    end
+  end 
+  
+  def doctors 
+    appointments.map 
   end
   
 end 
